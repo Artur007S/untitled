2408ef37451348bc961706395c0f50852070ccd4
Artur Piotr Sitarek <artur007@mbp-von-artur.speedport.ip>
2023-03-13 20:22:25 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+
+
+    fun main() {
+        println("Hello World!")
+        var Artur = "Helle World!"
+        var Artur2 = "Mein Name ist Artur"
+        var Hans = "$Artur $Artur2"
+        println("Artur, $Artur2")
+        println("Hallo wie heist du?(confirm with Enter)")
+        var name = readln()
+        println("Hallo $name!")
+        println()
+        println("Wie alt bist du?(confirm with Enter)")
+        var alter = readln()
+        println("Wow! Bereits $alter Jahre alt?!")
+
+    }
